cc4dc8db126a230e98351459264847adfea18cb4
shraddha-20 <shraddhaforcode@gmail.com>
2024-12-14 21:12:34 +0530

    first commit

---
+++ scripts/todo-list.js
@@ -0,0 +1,26 @@
+const todoList =['drive', 'swim', 'draw'];
+renderTodoList();
+
+function renderTodoList();
+ let todoListHTML = '';
+for(let i=0; i<todoList.length; i++){
+    const todo = todoList[i];
+    const html = `<p>${todo}<button> delete </button> </p>`;
+    todoListHTML += html;
+
+console.log(todoListHTML);
+
+
+document.querySelector('.js-todo-list')
+.innerHTML = todoListHTML;
+}
+function addTodo(){
+ const inputElement = document.querySelector('.js-name-input');
+ const name = inputElement.value;
+
+ todoList.push(name);
+ console.log(todoList);
+
+ inputElement.value='';
+ renderTodoList();
+}
